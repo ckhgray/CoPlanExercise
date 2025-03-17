1021cf1689fad848e566d16323b600ae3927ee00
grayc <psycg5@nottingham.ac.uk>
2025-03-17 20:57:53 +0000

    Added additional stats to capture but commented out printing

--- CoPlanLogAnalyser/Services/LogProcessor.cs
+++ CoPlanLogAnalyser/Services/LogProcessor.cs
@@ -8,34 +8,67 @@
                 return;
             }
 
-            var lines = File.ReadLines(filePath).Skip(1); //skipping header line
+            var lines = File.ReadLines(filePath).Skip(1); // skipping header line
 
+            int totalTasks = 0;
             int successfulQueryCount = 0;
             int successfulCommandCount = 0;
+            int failedQueryCount = 0;
+            int failedCommandCount = 0;
             long totalQueryTime = 0;
             long totalCommandTime = 0;
 
             foreach (var line in lines)
             {
-                if (Log.TryParse(line, out var log) && log.WasSuccess)
+                totalTasks++;
+
+                if (Log.TryParse(line, out var log))
                 {
-                    if (log.IsQuery)
+                    if (log.WasSuccess)
                     {
-                        successfulQueryCount++;
-                        totalQueryTime += log.TimeTaken;
+                        if (log.IsQuery)
+                        {
+                            successfulQueryCount++;
+                            totalQueryTime += log.TimeTaken;
+                        }
+                        else if (log.IsCommand)
+                        {
+                            successfulCommandCount++;
+                            totalCommandTime += log.TimeTaken;
+                        }
                     }
-                    else if (log.IsCommand)
+                    else
                     {
-                        successfulCommandCount++;
-                        totalCommandTime += log.TimeTaken;
+                        if (log.IsQuery)
+                        {
+                            failedQueryCount++;
+                        }
+                        else if (log.IsCommand)
+                        {
+                            failedCommandCount++;
+                        }
                     }
                 }
             }
 
+            int totalFailures = failedQueryCount + failedCommandCount;
+
+            // Displaying results
+            Console.WriteLine("----- Log File Statistics -----");
+            // Console.WriteLine($"Total Tasks Processed: {totalTasks}");
+            // Console.WriteLine();
+            
             Console.WriteLine($"Successful Query Tasks: {successfulQueryCount}");
-            Console.WriteLine($"Successful Command Tasks: {successfulCommandCount}");
+            //Console.WriteLine($"Failed Query Tasks: {failedQueryCount}");
             Console.WriteLine($"Average Time for Query Tasks: {AverageTime(totalQueryTime, successfulQueryCount)} ms");
+            Console.WriteLine();
+
+            Console.WriteLine($"Successful Command Tasks: {successfulCommandCount}");
+            //Console.WriteLine($"Failed Command Tasks: {failedCommandCount}");
             Console.WriteLine($"Average Time for Command Tasks: {AverageTime(totalCommandTime, successfulCommandCount)} ms");
+            Console.WriteLine();
+
+            // Console.WriteLine($"Total Failures: {totalFailures}");
         }
 
         private double AverageTime(long total, int count)
